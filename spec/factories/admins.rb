2404c982c6ee5c72311aa8bc36424fe3132b7496
aaronostroski <aaronmostroski@gmail.com>
2023-05-13 15:18:26 -0300

    feat

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :admin do
-    
+    name { 'Admin' }
+    sequence(:email) { |n| "#{n}_#{FFaker::Internet.email}" }
+    password { '123456' }
+
+    trait :active do
+      is_active { true }
+    end
   end
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  it 'Factory build should be valid'
-    expect(Factory.build(:admin)).to be_valid
+  it 'Factory build should be valid' do
+    expect(FactoryBot.build(:admin)).to be_valid
   end
 end
