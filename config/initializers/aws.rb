1c9e1b099afa1dbf1b2365eac2079ee63947d288
aaronostroski <aaronmostroski@gmail.com>
2023-05-14 21:54:52 -0300

    fix: CI

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,6 @@
-Aws.config.update({
-  region: "sa-east-1",
-  credentials: Aws::Credentials.new(Rails.application.credentials.s3[:key], Rails.application.credentials.s3[:secret]),
-})
+if Rails.env.production? && Rails.env.staging?
+  Aws.config.update({
+    region: "sa-east-1",
+    credentials: Aws::Credentials.new(Rails.application.credentials.s3[:key], Rails.application.credentials.s3[:secret]),
+  })
+end
