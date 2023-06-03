5ab897fd9663b3749db63a544442bf263529e93a
aaronostroski <aaronmostroski@gmail.com>
2023-06-03 18:53:55 -0300

    fix: products container on home and admin form

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -2,6 +2,6 @@ class Site::HomeController < SiteController
   def index
     @banners = Banner.active.order(order: :asc)
     @categories = Category.active.order(order: :asc)
-    @highlighted_products = Product.active.highlighted
+    @highlighted_products = Product.active.highlighted.limit(4)
   end
 end
