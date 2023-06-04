423e4b4fcab9ebc5ec4a0624a5f16b82d3099a48
aaronostroski <aaronmostroski@gmail.com>
2023-06-04 20:26:08 -0300

    feat: remove product from cart

---
+++ app/services/remove_product_from_cart.rb
@@ -0,0 +1,48 @@
+class RemoveProductFromCart
+  include ActiveModel::Model
+
+  attr_accessor :sgid, :order, :product, :product_id
+
+  validates :order, :order_products, :product_id, presence: true
+  validate :product_should_exist, :should_have_at_least_one_product_to_remove
+
+  def save
+    return false unless valid?
+
+    OrderProduct.transaction { order_products&.first&.destroy }
+
+    return false if errors.full_messages.any?
+
+    true
+  end
+
+  def create_sgid!
+    @sgid = order.to_sgid.to_s
+  end
+
+  def order
+    @order ||= GlobalID::Locator.locate_signed(sgid)
+  end
+
+  def order_products
+    @order_products ||= order.order_products.where(product:)
+  end
+
+  def product
+    @product ||= Product.active.find_by(id: product_id)
+  end
+
+  private
+
+  def total_products_by_choosed
+    order.order_products.where(product:).count
+  end
+
+  def product_should_exist
+    errors.add(:base, :product_doesnt_exist) unless product.present?
+  end
+
+  def should_have_at_least_one_product_to_remove
+    errors.add(:base, :no_products_to_remove) unless order_products.count > 0
+  end
+end
---
+++ spec/services/remove_product_from_cart_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe RemoveProductFromCart do
+  let(:product1) { FactoryBot.create(:product, :active) }
+  let(:product2) { FactoryBot.create(:product, :active) }
+  let(:order) { FactoryBot.create(:order, products: [product1, product2]) }
+  let(:sgid) { order.to_sgid.to_s }
+
+  describe 'Happy path' do
+    before { expect(order.order_products.count).to eql(2) }
+
+    it 'Should remove the product from cart' do
+      service = described_class.new(sgid:, product_id: product1.id)
+
+      expect(service).to be_valid
+      expect(service.save).to be true
+
+      expect(order.order_products.count).to eql(1)
+      expect(service.order.products).to_not include(product1)
+      expect(service.order.products).to include(product2)
+    end
+
+    it 'Can just one remove one product if there is more than 1' do
+      FactoryBot.create(:order_product, product: product1, order:)
+      expect(order.order_products.count).to eql(3)
+
+      service = described_class.new(sgid:, product_id: product1.id)
+
+      expect(service).to be_valid
+      expect(service.save).to be true
+
+      expect(order.order_products.count).to eql(2)
+      expect(order.products).to match_array([product1, product2])
+    end
+  end
+
+  describe 'Validations' do
+    it 'Product should exist' do
+      expect(Product.count).to eql(0)
+      fake_id = 1
+
+      service = described_class.new(sgid:, product_id: fake_id)
+
+      expect(service).to_not be_valid
+      expect(
+        service.errors.full_messages,
+      ).to include 'Este produto não existe no nosso sistema!'
+    end
+
+    it 'Product should be actives to be add on cart' do
+      not_active_product = FactoryBot.create(:product)
+      expect(not_active_product.is_active).to be false
+
+      service = described_class.new(sgid:, product_id: not_active_product.id)
+
+      expect(service).to_not be_valid
+      expect(
+        service.errors.full_messages,
+      ).to include 'Este produto não existe no nosso sistema!'
+    end
+
+    it 'Product to have remove should at least has one on the cart' do
+      product3 = FactoryBot.create(:product, :active)
+      service = described_class.new(sgid:, product_id: product3.id)
+
+      expect(service).to_not be_valid
+      expect(
+        service.errors.full_messages,
+      ).to include 'Não e possivel remover um produto que você não tem no carrinho!'
+    end
+  end
+end
