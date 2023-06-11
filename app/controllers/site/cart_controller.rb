1c596adcf08de9c06654c10ec0f40aecbeade7fe
aaronostroski <aaronmostroski@gmail.com>
2023-06-10 22:06:07 -0300

    feat: update cart counter

--- app/controllers/site/cart_controller.rb
+++ app/controllers/site/cart_controller.rb
@@ -5,19 +5,41 @@ class Site::CartController < SiteController
     if service.save
       session[:sgid] = service.sgid if sgid.nil?
       flash[:success] = I18n.t('.site.cart.flashs.successful_add_to_cart')
-      head(:ok)
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream:
+                   turbo_stream.update(
+                     "product_#{service.product.id}",
+                     partial: 'site/cart/total_products',
+                     locals: {
+                       product: service.product,
+                     },
+                   )
+        end
+      end
     else
-      flash[:error] = service.errors.full_messages.join(',')
       head(:bad_request)
+      flash[:error] = service.errors.full_messages.join(',')
     end
   end
 
   def remove_product
-    service = AddProductToCart.new(sgid:, product_id: params[:id])
+    service = RemoveProductFromCart.new(sgid:, product_id: params[:id])
 
     if service.save
       flash[:success] = I18n.t('.site.cart.flashs.successful_remove_from_cart')
-      head(:ok)
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream:
+                   turbo_stream.update(
+                     "product_#{service.product.id}",
+                     partial: 'site/cart/total_products',
+                     locals: {
+                       product: service.product,
+                     },
+                   )
+        end
+      end
     else
       flash[:error] = service.errors.full_messages.join(',')
       head(:bad_request)
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,3 +1,13 @@
 class SiteController < ApplicationController
   layout 'site'
+
+  helper_method :current_order
+
+  def current_order
+    @current_order ||= GlobalID::Locator.locate_signed(sgid) if sgid
+  end
+
+  def sgid
+    session[:sgid]
+  end
 end
