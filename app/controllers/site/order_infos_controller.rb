a8d55ae0e02f77cf49e3b9ca0a873037b1d1f4ff
aaronostroski <aaronmostroski@gmail.com>
2023-06-26 17:56:55 -0300

    refact: order to accept nested attributes order info

--- app/controllers/site/order_infos_controller.rb
+++ app/controllers/site/order_infos_controller.rb
@@ -1,10 +1,6 @@
 class Site::OrderInfosController < SiteController
-  before_action :set_order_info
-
   def create
-    @order_info.assign_attributes(form_params.to_h)
-
-    if @order_info.save
+    if current_order.update(order_info_attributes: form_params.to_h)
       flash[:success] = I18n.t('views.defaults.sucessfully_saved')
       redirect_to site_finish_order_path(step: 2)
     else
@@ -14,10 +10,6 @@ class Site::OrderInfosController < SiteController
   end
 
   private
-
-  def set_order_info
-    @order_info = OrderInfo.find_or_initialize_by(order: current_order)
-  end
 
   def form_params
     params.require(:order_info).permit(%i[first_name last_name email phone cpf])
